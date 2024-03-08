48553aa60dccb31d78c6fe94690634fe03b24e1b
MathieuCOLLARD <108083456+MathieuCOLLARD@users.noreply.github.com>
2024-03-08 17:38:44 +0100

    configure project (change dependencies in the back) and add first unitary test for login

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,6 +11,10 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { of, throwError } from 'rxjs';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { Router } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -38,5 +42,34 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  // Connexion
+  it('should login without error', () => {
+    const authservice = TestBed.inject(AuthService);
+    const login = jest.spyOn(authservice, 'login').mockImplementation(() => of({} as SessionInformation));
+    const sessionService = jest.spyOn(TestBed.inject(SessionService), 'logIn');
+    const route = jest.spyOn(TestBed.inject(Router), 'navigate').mockImplementation(async () => true);
+    component.submit()
+    expect(login).toHaveBeenCalled();
+    expect(sessionService).toHaveBeenCalled();
+    expect(route).toHaveBeenCalledWith((['/sessions']));
+  });
+
+  it('should not login if error', () => {
+    const authservice = TestBed.inject(AuthService);
+    const loginSpy = jest.spyOn(authservice, 'login').mockImplementation(() => throwError(() => new Error('err')));
+    component.submit()
+    expect(loginSpy).toHaveBeenCalled();
+    expect(component.onError).toBe(true);
+  });
+
+  it('should display an error message when wrong login', () => {
+    const authservice = TestBed.inject(AuthService);
+    jest.spyOn(authservice, 'login').mockImplementation(() => throwError(() => new Error('err')));
+    component.submit()
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.error').textContent).toContain('An error occurred');
   });
 });
