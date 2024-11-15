94b411390fff7b8771fe99ae8f0a217367512cf7
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-15 07:01:03 +0100

    Enhance UserService tests with additional cases
    Added tests for `getById` and `delete` methods to ensure proper functionality. Incorporated HttpClientTestingModule for mocking HTTP requests and verifying their correctness.

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -3,20 +3,39 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import { User } from '../interfaces/user.interface';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
-
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
-        HttpClientModule
+        HttpClientModule,
+        HttpClientTestingModule
       ]
     });
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should get by id', () => {
+    const mockUser: User = { id: 1, firstName: 'John', lastName: 'Doe' } as User;
+
+    service.getById('1').subscribe(user => {
+      expect(user).toEqual(mockUser);
+    });
+
+    const req: TestRequest = httpTestingController.expectOne(`api/user/1`);
+    expect(req.request.method).toBe('GET');
+  })
+  it('should delete by id', () => {
+    service.delete('1').subscribe(response => {
+      expect(response).toBeNull;
+    });
+  })
 });
