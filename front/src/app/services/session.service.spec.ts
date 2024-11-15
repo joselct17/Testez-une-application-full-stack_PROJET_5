1806c745dceac5c5bf162eee4c8b9aca949559be
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-15 07:00:38 +0100

    Add tests for SessionService login and logout functionality
    The tests include checks for initial states, login, logout, and observable $isLogged behavior. This ensures proper handling of session information and state transitions in the service.

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -14,4 +15,66 @@ describe('SessionService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should start with isLogged is false', () => {
+    expect(service.isLogged).toBeFalsy()
+  })
+
+  it('should set user into sessionInformation and isLogged to true', () => {
+    const mockSession: SessionInformation =  {
+      token: '1234',
+      type: 'Bearer',
+      id: 1,
+      username: 'JohnDoe',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: false
+    };
+    service.logIn(mockSession);
+    expect(service.isLogged).toBeTruthy()
+    expect(service.sessionInformation).toEqual(mockSession)
+  })
+
+  it('should set isLogged to false and sessionInformation to undefined', () => {
+    const mockSession: SessionInformation =  {
+      token: '1234',
+      type: 'Bearer',
+      id: 1,
+      username: 'JohnDoe',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: false
+    };
+    service.logIn(mockSession);
+    service.logOut()
+    expect(service.sessionInformation).toBeUndefined()
+    expect(service.isLogged).toBeFalsy()
+  })
+
+  // Manage async test with done method
+  it('should emit correct value from observable $isLogged', (done) => {
+    const mockSession: SessionInformation =  {
+      token: '1234',
+      type: 'Bearer',
+      id: 1,
+      username: 'JohnDoe',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: false
+    };
+
+    let asyncCheck = false
+    service.$isLogged().subscribe(isLogged => {
+      if(!asyncCheck){
+        expect(isLogged).toBeFalsy()
+        service.logIn(mockSession);
+        asyncCheck=true
+      }
+      else {
+        expect(isLogged).toBeTruthy()
+        done()
+      }
+    })
+    done()
+  })
 });
