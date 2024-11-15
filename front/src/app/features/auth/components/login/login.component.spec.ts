edfe3bd01f30c49e20a24af289c8f5d328ece445
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-15 07:01:11 +0100

    Add unit tests for LoginComponent
    Implemented mocks for AuthService and SessionService to simulate interactions. Added a test case to validate error handling for empty fields in the login form.

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,15 +11,44 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {of} from "rxjs";
 
+// Verify integration between LoginComponent, AuthService and SessionService
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockAuthService: Partial<AuthService>;
+  let mockSessionService: Partial<SessionService>;
+  let router: Router
 
   beforeEach(async () => {
+    // Mock the AuthService with a successful login response
+    mockAuthService = {
+      login: jest.fn().mockReturnValue(of({
+        token: '1234',
+        type: 'Bearer',
+        id: 1,
+        username: 'username',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: false
+      }))
+    };
+
+    // Mock the SessionService for handling session information
+    mockSessionService = {
+      logIn: jest.fn().mockReturnValue({})
+    };
+
+    // Configure the testing module
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        {provide: SessionService, useValue: mockSessionService},
+        {provide: AuthService, useValue: mockAuthService},
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -28,15 +57,32 @@ describe('LoginComponent', () => {
         MatIconModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule]
+        ReactiveFormsModule
+      ]
     })
       .compileComponents();
+
+    // Create the component instance and trigger change detection
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
+  // Verify that the component is created successfully
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display an error if one of the fields is empty', () => {
+    const email = component.form.get('email');
+    const password = component.form.get('password');
+
+    if (email && password) {
+      email.setValue('example@example.com');
+      password.setValue('');
+
+      expect(component.form.valid).toBe(false);
+    }
   });
 });
